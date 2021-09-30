4e7e3eb3141be08b47d42ed75b2d8a127f2f6cc5
Eric Brown <ewic98@gmail.com>
2021-09-30 16:15:23 +0200

    logistic regression model for titanic data

--- 04_exercise.py
+++ 04_exercise.py
@@ -3,6 +3,7 @@
 
 ### Ex1. Read in the titanic data attached to this exercise
 import pandas as pd
+from scipy.sparse import construct
 
 df = pd.read_csv('data/titanic.csv')
 
@@ -76,5 +77,39 @@ print('precision score: ', precision_score(X, pred, average='micro'))
 # recall
 from sklearn.metrics import recall_score
 print('recall score: ', recall_score(X, pred, average='micro'))
+# recall is the ratio of true positives over the sum of true positives and false negatives
 
 # f-score.
+from sklearn.metrics import f1_score
+# The F1 score can be interpreted as a weighted average of the precision and recall, 
+# where an F1 score reaches its best value at 1 and worst score at 0.
+print('f-score score: ', f1_score(X, pred, average='micro'))
+
+### Ex6. Train a logistic regression model with 'Survived' as the response using different subsets 
+# of the remaining columns as the explanatory variables
+
+X2 = df_train["Survived"].values
+Y2 = df_train[["Age", "Parents/Children Aboard"]].values
+
+X2 = X2.reshape(len(X2), 1)
+# Y2 = Y2.reshape(len(Y2), 1)
+
+# Use penalty='none' for the model. Which subset of variables gives the best results? 
+# Age & Parents/Children Aboard
+
+#                                   (independents, target)
+reg_train2 = LogisticRegression(penalty='none').fit(Y2, X2)
+
+pred2 = reg_train2.predict(Y2)
+
+print(pred2)
+
+rsq_err2 = mean_squared_error(X2, pred2)
+
+print("r-squared error prediction: ", rsq_err2)
+
+# Why do you think that metric is the most important in this case?
+print("I think the chances of survival were very dependant on a")
+print(" persons age and whether they had the perents/children aboard")
+print(" because of course the women/mother's and children were the first")
+print(" to get lifeboats.")
