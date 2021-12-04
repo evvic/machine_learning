2cd8b51ad99f349817cf455c0a19d8f0c6d90365
Eric Brown <ewic98@gmail.com>
2021-12-04 14:58:40 +0200

    decent attribute selection

--- project/README.md
+++ project/README.md
@@ -2,4 +2,7 @@
 # Predicting Students Final Grades
 
 ## Linear Regression Model
-Given all the attributes to potentially help predict the students' final grades are integers, using a linear regression model to find a best-fit line between these data points seems most optimal as the data directly correlates to each other. Since this model will be multivariate, it will also be multidimensional for each attribute used in predicting the final grade. 
+Given all the attributes to potentially help predict the students' final grades are integers, using a linear regression model to find a best-fit line between these data points seems most optimal as the data directly correlates to each other. Since this model will be multivariate, it will also be multidimensional for each attribute used in predicting the final grade.
+
+## Predictions
+To accurately predict the final grade, the first and second period grades could be used; but to make the project more interesting, these attributes will be ommitted to see if the model can accurately predict the final grade with other attributes, such as weekly alcohol consumption...
--- project/main.py
+++ project/main.py
@@ -26,11 +26,38 @@ data = pd.read_csv("data/student-mat.csv", sep=";")
 # EXPLORE THE DATA #
 ####################
 
-# filter to only desired attributes
-data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+#ex04
 
+# NOTE: what not to use: absences, (G1, G2 because they are too easy),
+
+# filter to only desired attributes
+data = data[["G3", "G1", "G2", "activities", "paid", "studytime", "Dalc", "Fedu", "Medu",  "famsup", "higher",
+    "Pstatus" ]]
+
+data['activities'] = data['activities'].map({'yes': 1, 'no': 0})
+data['paid'] = data['paid'].map({'yes': 1, 'no': 0})
+#data['schoolsup'] = data['schoolsup'].map({'yes': 1, 'no': 0})
+data['famsup'] = data['famsup'].map({'yes': 1, 'no': 0})
+data['higher'] = data['higher'].map({'yes': 1, 'no': 0})
+#data['internet'] = data['internet'].map({'yes': 1, 'no': 0})
+data['Pstatus'] = data['Pstatus'].map({'T': 1, 'A': 0})
 # set predict to the attribute we are trying to predict: final grade (G3)
 predict = "G3"
+
+# describe the final grade
+print("FINAL GRADES STATISTICS (", predict, ")\n", data[predict].describe(), '\n')
+
+### when finally decided which metrics to use, add more individual detail to each attribute when printing
+
+# print metrics of columns/attributes
+for col in data.columns:
+    if(col != predict):
+        print(col.upper(), 'STATISTICS\n', data[col].describe(), '\n')
+
+
+#######################
+# PREPROCESS THE DATA #
+#######################
 
 # drop the predict value fro the remaining attributes/features
 X = np.array(data.drop([predict], 1))
@@ -41,11 +68,34 @@ y = np.array(data[predict])
 # split data into testing and training sets
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
+########################
+# APPLY ML METHODS 1/2 #
+########################
 
 linear = linear_model.LinearRegression()
 
 # fitting the linear model to the training data
 linear.fit(x_train, y_train)
+
+# array of predictiosn for each student given the attributes
+predictions = linear.predict(x_test)
+
+# print each individual prediction with true value and attributes
+for x in range(len(predictions)):
+    if(predictions[x].round() == y_test[x]):
+        print(predictions[x].round(), '\t', y_test[x], "\tCORRECT!", x_test[x])
+    else:
+        print(predictions[x].round(), '\t', y_test[x], "\tX   \t", x_test[x])
+
+# get score of how correct the model was
+acc = linear.score(x_test, y_test)
+print("accuracy: ", acc)
+
+########################
+# APPLY ML METHODS 2/2 #
+########################
+
+# use a second method here????
 
 ###################
 # DISPLAY RESULTS #
@@ -56,11 +106,17 @@ from matplotlib import style
 # style scatterplot
 style.use("ggplot")
 
+# print metrics of columns/attributes
+for col in data.columns:
+    if(col != predict):
+        pyplot.scatter(data[col], data[predict], label=col)
 
-pyplot.scatter(data["G1"], data[predict])
-pyplot.scatter(data["G2"], data[predict])
-pyplot.scatter(data["absences"], data[predict])
-pyplot.xlabel("G1")
+#pyplot.scatter(data["G1"], data[predict], label="G1")
+#pyplot.scatter(data["G2"], data[predict], label="G2")
+#pyplot.scatter(data["absences"], data[predict])
+# pyplot.scatter(data["failures"], data[predict])
+pyplot.xlabel("Attributes")
 pyplot.ylabel("final grade")
+pyplot.legend(loc="upper left")
 
 pyplot.show()
